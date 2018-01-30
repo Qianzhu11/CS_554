bb7ad134d24f31e96c775c89d20fde5a306cf91b
ZZQ <2858583565@qq.com>
2018-01-31 07:37:36 +0800

    update

--- lab2/gulpfile.js
+++ lab2/gulpfile.js
@@ -1,4 +1,26 @@
 const gulp = require('gulp');
-const concatenate = require('gulp-concat');
-const gulpSASS = require('gulp-sass');
-const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+gulp.task('sass', () => {
+	return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+	.pipe(sass())
+	.pipe(gulp.dest('src/css'));
+	.pipe(browserSync.stream());
+});
+
+gulp.task('js', () => {
+	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.bootstrap.min.js', 'node_modules/jquery/dist/jquery.jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+	.pipe(gulp.dest('src/js'));
+});
+
+gulp.task('serve', ['sass'], () => {
+	browserSync.init({
+		server: './src'
+	});
+
+	gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+	gulp.watch('src/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['js', 'serve']);
