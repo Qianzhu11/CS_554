f8053c7986032f436bf0f278cc88dcae76f8745b
Qianzhu11 <2858583565@qq.com>
2018-01-31 15:00:17 -0500

    update

--- lab2/gulpfile.js
+++ lab2/gulpfile.js
@@ -5,13 +5,14 @@ const sass = require('gulp-sass');
 gulp.task('sass', () => {
 	return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
 	.pipe(sass())
-	.pipe(gulp.dest('src/css'));
+	.pipe(gulp.dest('src/css'))
 	.pipe(browserSync.stream());
 });
 
 gulp.task('js', () => {
-	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.bootstrap.min.js', 'node_modules/jquery/dist/jquery.jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-	.pipe(gulp.dest('src/js'));
+	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+	.pipe(gulp.dest('src/js'))
+	.pipe(browserSync.stream());
 });
 
 gulp.task('serve', ['sass'], () => {
